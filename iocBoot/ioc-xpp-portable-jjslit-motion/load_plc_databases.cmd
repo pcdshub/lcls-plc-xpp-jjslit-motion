5448911ccd49ceba9fca155ec331399e4d8c37f1
Aalayah I Spencer <spencera@slac.stanford.edu>
2022-07-26 13:26:39 -0700

    Updated IDs

--- iocBoot/ioc-xpp-portable-jjslit-motion/load_plc_databases.cmd
+++ iocBoot/ioc-xpp-portable-jjslit-motion/load_plc_databases.cmd
@@ -1,3 +1,3 @@
-# Total records: 42
-callbackSetQueueSize(2084)
+# Total records: 36
+callbackSetQueueSize(2072)
 dbLoadRecords("xpp_jjslit_motion.db", "PORT=ASYN_PLC,PREFIX=XPP:,IOCNAME=$(IOC),IOC=$(IOC)")
